dbb7bb191e5326b03acd10ae3cbbd2aeea2f3288
Nilay Singh <nilayy1234@gmail.com>
2018-08-07 13:34:59 +0530

    added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_121547) do
+ActiveRecord::Schema.define(version: 2018_08_07_074129) do
 
   create_table "agricultural_credit1s", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "Year"
